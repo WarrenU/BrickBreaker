c71bdd28b13b9cd59ec03812d2f8b2cad8dc992c
Warren U <warren.urbina@gmail.com>
2017-06-26 08:42:07 -0700

    update ball art style. Put in Collider script but not done.

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager level_manager;
+
+    void OnTriggerEnter2D(Collider2D trigger){
+    	print("Trigger");
+    	level_manager.load_level("Lose");
+    }
+
+    void OnCollisionEnter2D(Collider2D collision){
+    	print("col");
+    }
+}
