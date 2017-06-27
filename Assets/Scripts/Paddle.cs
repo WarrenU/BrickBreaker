cd021a21710139e15fe55e7ab45b2f156db76ea9
Warren U <warren.urbina@gmail.com>
2017-06-26 21:49:15 -0700

    Have paddle script that will moved based on mouse. Need to fix gameworld dimensions etc.

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 paddlePosition = new Vector3(0f, this.transform.position.y, 0f);
+		print(this.transform.position.y);
+		float mousePosition = (Input.mousePosition.x/Screen.width)*16;
+		paddlePosition.x = Mathf.Clamp(mousePosition, -6f, 6f);
+		this.transform.position = paddlePosition;
+	}
+}
