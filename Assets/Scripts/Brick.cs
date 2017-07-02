40a8ff2b6cfd0094e2c1dbb9d5df5e8ba4257292
Warren U <warren.urbina@gmail.com>
2017-07-02 12:06:47 -0700

    Put in edits to Brick hit counts, and made invulnerable bricks

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour {
-	public int maxHits;
-	private int timesHit;
 	public Sprite[] hitSprites;
+
+	private int timesHit;
 
 	// Use this for initialization
 	void Start () {
@@ -18,9 +18,28 @@ public class Brick : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
+		if(this.tag == "Breakable"){
+			HandleHits();
+		}
+	}
+
+	void HandleHits(){
+		int maxHits = hitSprites.Length + 1;
 		timesHit += 1;
 		if (timesHit >= maxHits){
 			Destroy(gameObject);
 		}
+		else{
+			LoadSprites();
+		}
 	}
+
+	void LoadSprites(){
+		int spriteIndex = timesHit - 1; // For 0 based index
+		if(hitSprites[spriteIndex]){
+			// if statement in place to make sure it doesn't set our sprite to None
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		}
+	}
+
 }
